a896d67a38a8ac88ae6471624fd69b17ba1aebfe
zc <798903520@qq.com>
2024-05-21 23:30:29 +0800

    uni-combox组件修改

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,6 +9,8 @@
       <span>体感温度:{{ weatherData.feelsLike }}</span>
       <span>能见度:{{ weatherData.vis }}</span>
       <span>大气压:{{ weatherData.pressure }}</span>
+      <span>更新时间:{{ weatherData.obsTime }}</span>
+      <uniCombox :candidates="objArr" @delete="getallData" @change="selectData" @input="getData($event, 'input')"></uniCombox>
       <t-button size="small" t-id="getWeather" @tap="getWeather()">获取天气</t-button>
     </view>
   </view>
@@ -20,8 +22,14 @@ import { http } from '../../api/http.js'
 import { weatherKey } from "../../static/weatherKey.js"
 
 import { useUserStore } from "../../store/modules/index.store.js"
+import uniCombox from './uni-combox.vue'
 
 let indexStore = useUserStore();
+
+let objArr = ref([]);
+setTimeout(() => {
+  objArr.value = [{ label: '1234', value: '1', key: '1' }, { label: '2345', value: '2', key: '2' }, { label: '3456', value: '2', key: '2' }];
+}, 1000);
 
 let title = computed(() => { return indexStore.GET_NAME});
 //当前天气
@@ -44,6 +52,16 @@ async function getWeather() {
   if (resList.code == 200) {
     weatherList.value = resList.daily;
   }
+}
+
+function getData(e, type=null){ 
+  console.log('e', e, type);
+}
+function getallData () { 
+  console.log('是时候获取所有数据了');
+}
+function selectData(params) {
+  console.log('params', params);
 }
 onMounted(() => {
   // getWeather();
